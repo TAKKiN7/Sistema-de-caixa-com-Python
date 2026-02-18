25d9052a32029dce0f607b4ec0655466dfe0ef7b
Eustáquio Navarro <eustaquionavarro467@gmail.com>
2026-02-17 22:11:44 -0300

    Retirado linhas desnecessárias do main

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from database.database import Banco
+from models.Caixa import Caixa
 from Interface import Janela
 
 
-banco: Banco = Banco()
-caixa = banco.obter_caixa()
-print(caixa.saldo)
-
+banco : Banco = Banco()
+caixa : Caixa = banco.obter_caixa()
 root : Janela = Janela(caixa, banco)
