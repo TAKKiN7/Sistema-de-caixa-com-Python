fc8e83d4af45781bbe1cd92cba046e5e339c2a5f
Eustaquio Navarro <eustaquionavarro467@gmail.com>
2026-02-21 14:47:30 -0300

    Adicionado comentários de explicação no main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from models.Caixa import Caixa
 from Interface import Janela
 
 
+# ojb Banco, responsavel por criar o ".db" e criar as informaçães do Caixa
 banco : Banco = Banco()
+
+# obj Caixa criado pelo banco apos obter as informações do caixa no ".db"
 caixa : Caixa = banco.obter_caixa()
+
+# inicializa a parte gráfica do Caixa
 root : Janela = Janela(caixa, banco)
