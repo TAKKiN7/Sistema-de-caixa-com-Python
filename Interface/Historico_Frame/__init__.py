9137e197de33733929952d162813dd80f0a5310f
Eustaquio Navarro <eustaquionavarro467@gmail.com>
2026-02-22 00:37:51 -0300

    Modificações na interface e Implementação da janela de Historico

--- Interface/Historico_Frame/__init__.py
+++ Interface/Historico_Frame/__init__.py
@@ -1,11 +1,81 @@
-from customtkinter import CTkFrame
+from customtkinter import CTkFrame, CTkToplevel, CTkButton
+from tkinter.ttk import Treeview, Style
 
 
-class Historico_Frame(CTkFrame):
+class Historico_Treeview(Treeview):
     def __init__(self, root):
-        super().__init__(root, border_width=2, bg_color="#1C2238", fg_color="#222A45", border_color="BLACK", corner_radius=20)
+        super().__init__(root, columns=("Valor", "Operação", "Data"), show="headings")
         self.layout()
 
 
     def layout(self):
-        self.place(relx=.25, rely=0.6, relwidth=.5, relheight=.25)
+        self.heading("Valor", text="Valor", anchor="w")
+        self.heading("Operação", text="Opereção", anchor="w")
+        self.heading("Data", text="Data", anchor="w")
+
+        for c in range(100):
+            self.insert("", "end", values=(c, "Entrada", "22/02/2026")) # Somente um teste antes de implementar historico no banco de dados
+
+        #self.pack(fill="both", expand=True)
+        self.place(relx=0, rely=0.1, relwidth=1, relheight=.9)
+    
+
+class Historico(CTkToplevel):
+    def __init__(self, root):
+        super().__init__(root)
+        self.configure(bg_color="BLACK", fg_color="BLACK")
+        self.config()
+        self.layout()
+
+
+    def config(self):
+        self.grab_set()
+        larguraT = self.winfo_screenwidth()
+        alturaT = self.winfo_screenheight()
+        self.overrideredirect(True)
+        self.geometry(f"520x330+{int((larguraT / 2) - (520 / 2))}+{int((alturaT / 1.65))}")
+
+    
+    def layout(self):
+        fecharB : CTkButton = CTkButton(self, text="Fechar", fg_color="RED", corner_radius=0,
+                                        command=self.fechar,border_width=2, border_color="BLACK",
+                                        hover_color="#5c0a12")
+        fecharB.place(relx=0, rely=0, relwidth=1, relheight=0.1)
+        self.style()
+        tree : Treeview = Historico_Treeview(self)
+
+
+    def fechar(self):
+        self.destroy()
+
+    def style(self):
+        estilo = Style()
+        estilo.theme_use("default")
+
+        estilo.configure(
+            "Treeview",
+            foreground="WHITE",
+            background="#1C2238",
+            fieldbackground="#1C2238",
+            bordercolor="red",
+            borderwidth=3
+        )
+
+        estilo.map(
+            "Treeview",
+            background=[("selected", "BLACK")],
+            foreground=[("selected", "white")]
+        )
+
+        estilo.configure(
+            "Treeview.Heading",
+            background="#1C2238",
+            foreground="white",
+            relief="flat"
+        )
+
+        estilo.map(
+            "Treeview.Heading",
+            background=[("selected", "BLACK")],
+            foreground=[("selected", "WHITE")]
+        )
--- Interface/__init__.py
+++ Interface/__init__.py
@@ -4,7 +4,7 @@ from Interface.Borda_Frame import Borda
 from models.Caixa import Caixa
 from database.database import Banco
 from Interface.Saldo_Frame import Saldo_Frame
-from Interface.Historico_Frame import Historico_Frame
+from Interface.Historico_Frame import Historico
 from Interface.Janela_Acao import JanelaAcaoEntrada, JanelaAcaoSaida
 
 
@@ -29,34 +29,32 @@ class Janela(CTk):
         nomeL.place(relx=0, rely=.0, relwidth=1)
 
         self.frame_saldo()
-        self.frame_historico()
 
-        entradaB : CTkButton = CTkButton(self, text="Entrada", command=self.entrada, font=("itim", 20, "bold"), border_width=0, fg_color="#0f0",
-                                   text_color="WHITE")
-        entradaB.place(relx=.26, rely=0.25, relwidth=.15)
+        entradaB : CTkButton = CTkButton(self, text="Entrada", command=self.entrada, font=("itim", 20, "bold"), border_width=0, fg_color="#00ff00",
+                                   text_color="WHITE", hover_color="#0ab90a")
+        entradaB.place(relx=.26, rely=0.25, relwidth=.15, relheight=.06)
 
-        saidaB: CTkButton = CTkButton(self, text="Saída", command=self.saida, font=("itim", 20, "bold"), border_width=0, fg_color="#f00",
-                                text_color="WHITE")
-        saidaB.place(relx=.42, rely=.25, relwidth=.16)
+        saidaB: CTkButton = CTkButton(self, text="Saída", command=self.saida, font=("itim", 20, "bold"), border_width=0, fg_color="#ff0000",
+                                text_color="WHITE", hover_color="#c70606")
+        saidaB.place(relx=.42, rely=.25, relwidth=.16, relheight=.06)
 
-        relatorioB: CTkButton = CTkButton(self, text="Relatório", font=("itim", 20, "bold"), border_width=0, fg_color="#00f",
-                                    text_color="WHITE")
-        relatorioB.place(relx=.59, rely=.25, relwidth=.15)
+        relatorioB: CTkButton = CTkButton(self, text="Relatório", font=("itim", 20, "bold"), border_width=0, fg_color="#0000ff",
+                                    text_color="WHITE", hover_color="#0000c7",
+                                    command=self.historico)
+        relatorioB.place(relx=.59, rely=.25, relwidth=.15, relheight=.06)
 
         self.fechar_button()
-
-
-    def frame_historico(self):
-        historico_frame : CTkFrame = Historico_Frame(self)
 
     def frame_saldo(self):
         self.saldo: StringVar = StringVar()
         self.saldo.set(self.saldo_formatado())
         self.saldoF: CTkFrame = Saldo_Frame(self, self.saldo)
 
+
     def fechar_button(self):
-        sairB: CTkButton = CTkButton(self, text="Sair", font=("itim", 10, "bold"),
-                               fg_color="#9b111e", text_color="WHITE", command=self.fechar, border_width=0)
+        sairB: CTkButton = CTkButton(self, text="Sair", font=("itim", 13, "bold"),
+                               fg_color="#9b121f", hover_color="#5c0a12", text_color="WHITE",
+                               command=self.fechar, border_width=0)
         sairB.place(relx=.9, rely=.01, relwidth=.08)
 
 
@@ -89,6 +87,9 @@ class Janela(CTk):
     def atualizar_saldo(self):
         self.saldo.set(self.saldo_formatado())
 
+    def historico(self):
+        self.janela_historico : Historico = Historico(self)
+       
     def entrada(self):
         janela : JanelaAcaoEntrada = JanelaAcaoEntrada(self, self.caixa, self.banco, self.atualizar_saldo)
         #self.caixa.entrada(100)
